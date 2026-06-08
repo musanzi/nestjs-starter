753d675d3b14fcbd93335500fea01044ada79820
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-08 16:26:30 +0200

    refactor(auth): remove unused AuthService and clean up module imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,5 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
-import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UsersModule } from '../users/users.module';
@@ -9,25 +8,15 @@ import { Role } from '../roles/entities/role.entity';
 import { User } from '../users/entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { SessionSerializer } from './session.serializer';
+import { SessionSerializer } from './serializers/session.serializer';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CommandHandlers } from './commands/handlers';
 import { QueryHandlers } from './queries/handlers';
 import { EventHandlers } from './events/handlers';
 
-@Global()
 @Module({
   imports: [CqrsModule, UsersModule, TypeOrmModule.forFeature([User, Role]), PassportModule, JwtModule],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    LocalStrategy,
-    GoogleStrategy,
-    SessionSerializer,
-    ...CommandHandlers,
-    ...QueryHandlers,
-    ...EventHandlers
-  ],
-  exports: [AuthService]
+  providers: [LocalStrategy, GoogleStrategy, SessionSerializer, ...CommandHandlers, ...QueryHandlers, ...EventHandlers]
 })
 export class AuthModule {}
