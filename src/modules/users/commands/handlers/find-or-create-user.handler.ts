88bf6f47843d83687ddb41e24d33f8a3a068eadd
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-09 10:42:10 +0200

    refactor(users): streamline user retrieval logic and remove unused query handling

--- src/modules/users/commands/handlers/find-or-create-user.handler.ts
+++ src/modules/users/commands/handlers/find-or-create-user.handler.ts
@@ -1,30 +1,34 @@
-import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
-import { CommandBus, CommandHandler, ICommandHandler, QueryBus } from '@nestjs/cqrs';
+import { BadRequestException, Logger } from '@nestjs/common';
+import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { logHandlerError } from '@/shared/helpers';
 import { UserResponse } from '../../interfaces';
-import { FindUserByEmailQuery } from '../../queries';
 import { FindOrCreateUserCommand } from '../impl/find-or-create-user.command';
 import { UpdateUserCommand } from '../impl/update-user.command';
 import { CreateUserCommand } from '../impl/create-user.command';
+import { Repository } from 'typeorm';
+import { User } from '../../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @CommandHandler(FindOrCreateUserCommand)
 export class FindOrCreateUserHandler implements ICommandHandler<FindOrCreateUserCommand, UserResponse> {
   private readonly logger = new Logger(FindOrCreateUserHandler.name);
 
   constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+    private readonly commandBus: CommandBus
   ) {}
 
   async execute(command: FindOrCreateUserCommand): Promise<UserResponse> {
     const { dto } = command;
 
     try {
-      try {
-        const existingUser = await this.queryBus.execute(new FindUserByEmailQuery(dto.email));
+      const existingUser = await this.repository.findOne({
+        where: { email: dto.email }
+      });
+
+      if (existingUser) {
         return this.commandBus.execute(new UpdateUserCommand(existingUser.id, dto));
-      } catch (error) {
-        if (!(error instanceof NotFoundException)) throw error;
       }
 
       return await this.commandBus.execute(new CreateUserCommand(dto));
