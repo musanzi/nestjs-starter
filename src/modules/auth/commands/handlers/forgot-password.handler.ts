e1589b835a95a405dac42253b0506c9990956322
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-09 21:15:11 +0200

    fix: add a newline for improved code readability in ForgotPasswordHandler

--- src/modules/auth/commands/handlers/forgot-password.handler.ts
+++ src/modules/auth/commands/handlers/forgot-password.handler.ts
@@ -29,6 +29,7 @@ export class ForgotPasswordHandler implements ICommandHandler<ForgotPasswordComm
 
       const frontendUri = this.configService.get<string>('FRONTEND_URI');
       const link = `${frontendUri}/reset-password?token=${token}`;
+
       this.eventBus.publish(new ResetPasswordRequestedEvent(user, link));
     } catch (error) {
       logHandlerError(this.logger, 'Forgot password', error, `email="${dto.email}"`);
