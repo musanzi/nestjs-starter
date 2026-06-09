26b0f9c40c11d4730a24e5332cb3b4c7154053c5
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-10 00:31:58 +0200

    refactor: streamline FindUserQuery execution in DeleteUserHandler

--- src/modules/users/commands/handlers/delete-user.handler.ts
+++ src/modules/users/commands/handlers/delete-user.handler.ts
@@ -19,9 +19,7 @@ export class DeleteUserHandler implements ICommandHandler<DeleteUserCommand, voi
 
   async execute(command: DeleteUserCommand): Promise<void> {
     try {
-      await this.queryBus.execute(
-        new FindUserQuery({ id: command.id })
-      );
+      await this.queryBus.execute(new FindUserQuery({ id: command.id }));
 
       await this.repository.softDelete(command.id);
     } catch (error) {
