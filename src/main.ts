10982afdf25cff0c9f281ab3cef3fc4b0d1bc006
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-11 14:51:25 +0200

    feat: integrate PostgreSQL session store with express-session

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,26 @@ import { ValidationPipe } from '@nestjs/common';
 import session from 'express-session';
 import passport from 'passport';
 import { Logger } from 'nestjs-pino';
+import connectPgSimple from 'connect-pg-simple';
+import { Pool } from 'pg';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const isProduction = process.env.NODE_ENV === 'production';
+  const PgSessionStore = connectPgSimple(session);
+  const sessionStore = new PgSessionStore({
+    pool: new Pool({
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    }),
+    tableName: 'session',
+    createTableIfMissing: true
+  });
+
   app.useLogger(app.get(Logger));
   app.enableCors({
     credentials: true,
@@ -20,6 +37,8 @@ async function bootstrap(): Promise<void> {
   );
   app.use(
     session({
+      store: sessionStore,
+      name: 'sid',
       secret: process.env.SESSION_SECRET,
       saveUninitialized: false,
       resave: false,
@@ -27,6 +46,7 @@ async function bootstrap(): Promise<void> {
       cookie: {
         maxAge: +process.env.SESSION_MAX_AGE,
         httpOnly: true,
+        secure: isProduction,
         sameSite: 'lax'
       }
     })
